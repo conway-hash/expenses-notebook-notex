bff8aa4f637edc4374216bdf98ede367c14073cd
Tomáš Bokor <tomasbokor.dev@gmail.com>
2024-09-16 18:41:43 +0200

    2.2.1 - env var bugfix

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const apiKey = process.env.MEDIA_STACK_API_KEY;
-  const country = 'sk'
-  const q = 'electricity'
+  // const country = 'sk'
+  // const q = 'electricity'
 
   const url = `http://api.mediastack.com/v1/news?access_key=${apiKey}&countries=sk`;
 
